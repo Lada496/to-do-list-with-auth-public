11fda0fcbf2503a5df39541d537b3eb1b126ccda
Lada496 <67321065+Lada496@users.noreply.github.com>
2021-10-28 22:23:37 -0700

    modified scss

--- src/config/config_example.js
+++ src/config/config_example.js
@@ -1,9 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "api-key",
-    authDomain: "auth-domain",
-    projectId: "project-id",
-    storageBucket: "storage-bucket",
-    messagingSenderId: "messaging-sender-id",
-    appId: "app-id",
-    measurementId: "measurement-id",
-  };
+  apiKey: "api-key",
+  authDomain: "auth-domain",
+  projectId: "project-id",
+  storageBucket: "storage-bucket",
+  databaseURL: "https://databaseURL",
+  messagingSenderId: "messaging-sender-id",
+  appId: "app-id",
+  measurementId: "measurement-id",
+};
